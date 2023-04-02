148955c8bb96beec93cc5ff238fd2f4c2a460c8f
NatanaelBilly <if-20011@students.ithb.ac.id>
2023-04-02 17:00:32 +0700

    add response controller

--- API/model/response_model.go
+++ API/model/response_model.go
@@ -1,6 +1,28 @@
 package model
 
-type GeneralResponse struct {
-	Status  int    `json:"Status"`
-	Message string `json:"Message"`
+type Pengguna struct {
+	Id_pengguna   int    `json:"id_pengguna"`
+	Nama          string `json:"nama"`
+	Email         string `json:"email"`
+	Password      string `json:"password"`
+	Jenis_kelamin int    `json:"jenis_kelamin`
+	Tanggal_lahir string `json:tanggal_lahir`
+	Nomor_telepon int    `json:"nomor_telepon"`
+	Alamat        string `json:"alamat"`
+}
+type MessageResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+type UserResponse struct {
+	Status  int      `json:"status"`
+	Message string   `json:"message"`
+	Data    Pengguna `json:"data"`
+}
+
+type UsersResponse struct {
+	Status  int        `json:"status"`
+	Message string     `json:"message"`
+	Data    []Pengguna `json:"data"`
 }
