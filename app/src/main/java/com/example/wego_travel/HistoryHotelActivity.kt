6592be1853530b170bfc7a8d78144718bf0922ac
Hans <hansjonathank22@gmail.com>
2023-04-10 15:14:02 +0700

    menambah navigation ke lihat history hotel

---
+++ app/src/main/java/com/example/wego_travel/HistoryHotelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wego_travel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HistoryHotelActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.history_hotel_page)
+    }
+}
