1432132c468a8904e9f27ccc12d32aff27465e7b
Hans <hansjonathank22@gmail.com>
2023-04-10 15:01:24 +0700

    menambah navigation ke update profile

--- app/src/main/java/com/example/wego_travel/MenuActivity.kt
+++ app/src/main/java/com/example/wego_travel/MenuActivity.kt
@@ -3,6 +3,8 @@ package com.example.wego_travel
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -26,9 +28,32 @@ class MenuActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(navAirplane)
             }
             R.id.img_hotel -> {
-                val navHotel = Intent(this, MainActivity::class.java)
+                val navHotel = Intent(this, UpdateProfileActivity::class.java)
                 startActivity(navHotel)
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.update_profile -> {
+                val navAirplane = Intent(this, UpdateProfileActivity::class.java)
+                startActivity(navAirplane)
+            }
+            R.id.lihat_history_pesawat -> {
+                val navHotel = Intent(this, UpdateProfileActivity::class.java)
+                startActivity(navHotel)
+            }
+            R.id.lihat_history_hotel -> {
+                val navHotel = Intent(this, UpdateProfileActivity::class.java)
+                startActivity(navHotel)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
